4b4b934cb81f3c6581f13967603b28dffd700fde
mail-aizhan <65739091+mail-aizhan@users.noreply.github.com>
2023-12-29 17:26:49 +0600

    added css,js

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-let firsCard = 6
+/*let firstCard = 6
 let secondCard =9
 
-let sum = firstCard + secondCard 
+let sum = firstCard + secondCard +6
+
 
 if (sum < 21){
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
-    console.loh(" wohoo! you have got Blackjack")
+    console.log(" wohoo! you have got Blackjack")
+}else if ( sum > 21) {
+    console.log("You are out of the game!")
 }
+
+
+let age = 19
+
+if (age <21) {
+    console.log(" You can not enter")
+} else { 
+        console.log("you are welcome ")
+    }
+*/
+
+let age = 102
+if (age < 100){
+    console.log("You are not eligible!")}
+    else if (age === 100){
+        console.log ("Congratulations, you won the birthday card from the King !")
+        } else { 
+            console.log("you have already gotten the birthday card")
+    }
