ffdd7e2621b91c1ca5d3bd7023f2911ee56662a9
mail-aizhan <65739091+mail-aizhan@users.noreply.github.com>
2023-12-29 15:30:26 +0600

    added js

---
+++ index.js
@@ -0,0 +1,10 @@
+let firsCard = 6
+let secondCard =9
+
+let sum = firstCard + secondCard 
+
+if (sum < 21){
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.loh(" wohoo! you have got Blackjack")
+}
